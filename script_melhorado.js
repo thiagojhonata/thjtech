c81ca4c762d019881a5f0f3368ac19109cc12f53
Thiago Jhonata <thiagojhonata@outlook.com>
2026-06-16 00:21:31 -0300

    Atualização

---
+++ script_melhorado.js
@@ -0,0 +1,9 @@
+// Efeito suave ao rolar para seções
+document.querySelectorAll('a[href^="#"]').forEach(anchor => {
+  anchor.addEventListener('click', function (e) {
+    e.preventDefault();
+    document.querySelector(this.getAttribute('href')).scrollIntoView({
+      behavior: 'smooth'
+    });
+  });
+});
